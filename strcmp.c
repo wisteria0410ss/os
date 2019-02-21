70ef61d745cdbdae0cba143ce43df9763b5c141f
wisteria0410ss <wisteria0410ss@gmail.com>
2019-02-21 15:10:51 +0900

    strcmp簡略化

--- strcmp.c
+++ strcmp.c
@@ -2,13 +2,8 @@
 
 int strcmp(const char *s1, const char *s2){
     for(int i=0;;i++){
-        char s, t;
-        if(s1[i] == '\0') s = 0;
-        else s = s1[i];
-        if(s2[i] == '\0') t = 0;
-        else t = s2[i];
-        if(s > t) return 1;
-        if(s < t) return -1;
-        if(s == 0 && t == 0) return 0;
+        if(s1[i] > s2[i]) return 1;
+        if(s1[i] < s2[i]) return -1;
+        if(s1[i] == 0 && s2[i] == 0) return 0;
     }
 }
