1dabbef4b4e281f27880b9534a4a75c29b0ad46c
wisteria0410ss <wisteria0410ss@gmail.com>
2019-02-25 23:06:54 +0900

    21日目-6

--- app/crack.c
+++ app/crack.c
@@ -1,4 +1,11 @@
 void app_main(){
-    *((char *) 0x00102600) = 0;
+    __asm__ volatile(
+        "mov    eax, 1*8\n"
+        "mov    ds, ax\n"
+        "movb   [0x102600], 0"
+        :
+        :
+        :"eax"
+    );
     return;
 }
