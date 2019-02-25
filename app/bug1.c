a74aa98166597fb912f1bd575fee15244f8df28b
wisteria0410ss <wisteria0410ss@gmail.com>
2019-02-26 06:04:25 +0900

    微修正

--- app/bug1.c
+++ app/bug1.c
@@ -6,8 +6,8 @@ void app_main(){
     api_putchar(a[10]);
     a[102] = 'B';
     api_putchar(a[102]);
-    a[123] = 'C';
-    api_putchar(a[123]);
+    a[12345678] = 'C';
+    api_putchar(a[12345678]);
     api_putchar('\n');
     api_end();
 }
